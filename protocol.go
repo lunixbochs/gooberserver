a385210e4602a1a641cc7672304943fa3b3383c8
Ryan Hileman <lunixbochs@gmail.com>
2016-02-20 19:57:56 -0800

    add dynamic protocol dispatch

--- protocol.go
+++ protocol.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	// "strings"
+	"reflect"
+	"strings"
 )
 
 type Protocol struct {
@@ -17,6 +18,47 @@ func (p *Protocol) addClient() {
 func (p *Protocol) removeClient() {
 }
 
+func (p *Protocol) Respond(msg string) {
+	// TODO: add msg id responder
+	p.Client.Send(msg)
+}
+
 func (p *Protocol) Handle(msg string) {
-	fmt.Println("protocol handler:", msg)
+	split := strings.Split(msg, " ")
+	// TODO: parse out msg ids
+	cmd := strings.ToUpper(split[0])
+	args := split[1:]
+
+	method := reflect.ValueOf(p).MethodByName(cmd)
+	if !method.IsValid() {
+		// TODO: check if DENIED is the right response
+		p.Respond(fmt.Sprintf("DENIED Command not found: %s", cmd))
+		return
+	}
+	numIn := method.Type().NumIn()
+	if numIn > len(args) {
+		p.Respond(fmt.Sprintf("DENIED Not enough arguments to %s", cmd))
+		return
+	} else if numIn < len(args) {
+		extra := args[numIn-1:]
+		args = args[:numIn-1]
+		args = append(args, strings.Join(extra, " "))
+	}
+	argVals := make([]reflect.Value, len(args))
+	for i, v := range args {
+		argVals[i] = reflect.ValueOf(v)
+	}
+	// TODO: errors?
+	// TODO: per-battle/channel acls
+	acl := p.Client.Acl()
+	if err := acl.Check(cmd, 0, 0); err != nil {
+		// TODO: better message
+		p.Respond(fmt.Sprintf("DENIED %s", err))
+		return
+	}
+	method.Call(argVals)
+}
+
+func (p *Protocol) LOGIN(user, pass string) {
+	fmt.Println("LOGIN", user, pass)
 }
